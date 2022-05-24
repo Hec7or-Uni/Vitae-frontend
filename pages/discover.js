a17a07704e45152402c59c9a8c315af1b7d5c522
hec7orci7o <hectortoraltrabajos@gmail.com>
2022-05-24 02:50:47 +0200

    almost finish

--- pages/discover.js
+++ pages/discover.js
@@ -2,14 +2,14 @@ import Layout from '../components/Layout/WithSession'
 import { getSession } from 'next-auth/react'
 import Card from '../components/Card'
 import Search from '../components/Search'
-import { data } from '../lib/temp'
+// import { data } from '../lib/temp'
 
 export default function Discover ({ recipes }) {
   return (
     <div className='max-w-5xl flex flex-col gap-5'>
       <Search />
       <div className='flex flex-wrap gap-4 overflow-y-auto'>
-        {recipes.map(item => <Card key={item.id} id={item.id} img={item.image} />)}
+        {recipes.map(item => <Card key={item.spoonId} id={item.spoonId} img={item.image} />)}
       </div>
     </div>
   )
@@ -30,13 +30,15 @@ export async function getServerSideProps ({ req }) {
       }
     }
   }
-  const recipes = data.recipes
+  // const recipes = data.recipes
 
-  // const recipes = await fetch('http://localhost:4000/api/inventory/random-recipes', {
-  //   headers: {
-  //     Authorization: `Bearer ${req.cookies['next-auth.session-token']}`
-  //   }
-  // }).then(res => res.json())
+  const params = new URLSearchParams({ email: session.user.email })
+  const recipes = await fetch(`http://localhost:4000/api/inventory/random-recipes?${params}`, {
+    headers: {
+      Authorization: `Bearer ${req.cookies['next-auth.session-token']}`
+    }
+  }).then(res => res.json())
+  console.log(recipes)
 
   return {
     props: { recipes }
