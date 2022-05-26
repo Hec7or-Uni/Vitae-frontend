e0f6802dbf37d3a35267524602e3d9110c1b23e9
hec7orci7o <hectortoraltrabajos@gmail.com>
2022-05-26 16:20:46 +0200

    login realcionado con home problem?

--- pages/home.js
+++ pages/home.js
@@ -94,11 +94,11 @@ export async function getServerSideProps ({ req }) {
     }
   }
 
-  await fetch(`${process.env.NEXT_PUBLIC_BASE_PATH_BACKEND}user/statistics`, {
-    method: 'PUT',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ field: 'visitIndex' })
-  }).catch(err => console.error(err))
+  // await fetch(`${process.env.NEXT_PUBLIC_BASE_PATH_BACKEND}user/statistics`, {
+  //   method: 'PUT',
+  //   headers: { 'Content-Type': 'application/json' },
+  //   body: JSON.stringify({ field: 'visitIndex' })
+  // }).catch(err => console.error(err))
 
   const parametros = new URLSearchParams({ email: session.user.email })
   const user = await fetch(`${process.env.NEXT_PUBLIC_BASE_PATH_BACKEND}user?${parametros}`, {
